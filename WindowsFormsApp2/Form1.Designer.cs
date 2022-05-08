281a6ab072bff58eeac9d02911d3f62f9b968e85
stari <stari@DESKTOP-7IQEQL4>
2022-05-08 17:18:50 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(32, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(226, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(424, 23);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 89);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(606, 23);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.domainUpDown1.TabIndex = 3;
+            this.domainUpDown1.Text = "domainUpDown1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            List<Phone> phones = new List<Phone>
+        {
+            new Phone { Id=11, Name="Samsung Galaxy Ace 2", Year=2012},
+            new Phone { Id=12, Name="Samsung Galaxy S4", Year=2013},
+            new Phone { Id=13, Name="iPhone 6", Year=2014},
+            new Phone { Id=14, Name="Microsoft Lumia 435", Year=2015},
+            new Phone { Id=15, Name="Xiaomi Mi 5", Year=2015}
+        };
+
+            comboBox1.DataSource = phones;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "Id";
+
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+
+            listBox1.DataSource = phones;
+            listBox1.DisplayMember = "Name";
+            listBox1.ValueMember = "Id";
+
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+
+            checkedListBox1.MultiColumn = true;
+            checkedListBox1.DataSource = phones;
+            checkedListBox1.DisplayMember = "Name";
+            checkedListBox1.ValueMember = "Id";
+
+            checkedListBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+
+            List<string> states = new List<string>
+        {
+            "Аргентина", "Бразилия", "Венесуэла", "Колумбия", "Чили"
+        };
+
+            // добавляем список элементов
+            domainUpDown1.Items.AddRange(states);
+            domainUpDown1.TextChanged += domainUpDown1_TextChanged;
+        }
+
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Phone phone = (Phone)comboBox1.SelectedItem;
+            listBox1.Items.Add(phone);
+        }
+        void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // получаем id выделенного объекта
+            int id = (int)listBox1.SelectedValue;
+
+            // получаем весь выделенный объект
+            Phone phone = (Phone)listBox1.SelectedItem;
+            MessageBox.Show(id.ToString() + ". " + phone.Name);
+        }
+        void checkedlistBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // получаем id выделенного объекта
+            int id = (int)checkedListBox1.SelectedValue;
+
+            // получаем весь выделенный объект
+            Phone phone = (Phone)checkedListBox1.SelectedItem;
+        }
+
+        void domainUpDown1_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(domainUpDown1.Text);
+        }
+    }
+    class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}
